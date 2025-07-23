c049e1937751a1ceef1e50250c4aafeeee489e54
aandroide <14141574+aandroide@users.noreply.github.com>
2025-07-23 21:32:59 +0200

    Update default.py

--- default.py
+++ default.py
@@ -73,13 +73,13 @@ class ApiWarningDialog(xbmcgui.WindowXMLDialog):
 
     def onInit(self):
         # Imposta il titolo
-        self.setProperty("repo_name", self.repo_name)
+        self.setProperty("YouTube", self.repo_name)
         
         message = (
             f"Per il corretto funzionamento dell'addon [B]{self.repo_name} è necessario[/B]:\n\n"
-            "1. Essere in possesso/creare un account Google\n"
-            "2. Seguire la guida per la creazione delle chiavi API, scansionando il QR Code\n"
-            "3. Essere consapevoli che in assenza di un account Google e la creazione delle relative API l'addon avrà funzionalità limitate\n"
+            "1. Essere in possesso/creare un account Google.\n"
+            "2. Seguire la guida per la creazione delle chiavi API, scansionando il QR Code/link.\n"
+            "3. Essere consapevoli che in assenza di un account Google e la creazione delle relative API l'addon avrà funzionalità limitate.\n"
         )
         self.getControl(200).setText(message)
         
